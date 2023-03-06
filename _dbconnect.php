b640d0937e2ec5e8a97f673a68a1db7c2eaf2fdd
Samaksh Bhatnagar <112722706+samaksh249@users.noreply.github.com>
2023-03-06 20:03:28 +0530

    Add files via upload

---
+++ _dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+//Script to connect to the data base
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "idiscuss";
+
+$conn = mysqli_connect($servername, $username, $password,$database);
+
+?>
